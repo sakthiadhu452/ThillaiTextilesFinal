1932cd23250b98ff74323b709ba3c68d15a9670f
SAKTHI ADHAVAN K <sakthiadhavank.21eie@kongu.edu>
2024-09-24 21:38:36 +0530

    Update vite.config.js

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
-    host:true,
-    port: 80
+  server: {
+    host: '13.60.50.147',  // Replace with your EC2 instance public IP
+    port: 5173,            // Change the port to something higher than 1024
+    strictPort: true       // Ensure the port is strictly used
   }
 })
