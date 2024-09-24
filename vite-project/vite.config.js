aafa7764c1137dc2aeed560ab0db30ae47d2ae81
SAKTHI ADHAVAN K <sakthiadhavank.21eie@kongu.edu>
2024-09-24 21:56:06 +0530

    Save local changes before merging

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -6,7 +6,22 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host:true,
-    port: 80
+    port: 3000
   }
 })
+
+
+
+// import { defineConfig } from
+//  'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',  // Bind to all interfaces
+//     port: 5173,        // Specify the port
+//     strictPort: true
+//   }
+// })
